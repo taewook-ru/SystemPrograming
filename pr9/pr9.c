5b92be65ae67b81145ee21e361e6b3ffb67d1eb0
taewook-ru <xodnr106@naver.com>
2019-10-21 23:21:38 -0700

    Lab2

---
+++ pr9/pr9.c
@@ -0,0 +1,66 @@
+#include <search.h>
+#include <stdio.h>
+#include <string.h>
+
+struct node {
+
+	char *name;
+	int mid, fin;
+};
+
+#define TABLESIZE 50
+
+char nametable[TABLESIZE*20];
+struct node nodetable[TABLESIZE];
+struct node *root = NULL;
+
+int compare(const void *cp1, const void *cp2, const void *cp3)
+{
+	return strcmp(((struct node *)cp1)->name, ((struct node *)cp2)->name);
+}
+
+void print_node(const void*, VISIT, int);
+
+int main()
+{
+	char *nameptr = nametable;
+	struct node *nodeptr = nodetable;
+	struct node **ret;
+	int i = 1;
+	int a;
+
+	printf("input count of student");
+	scanf("%d", &a);
+
+	ret = (struct node **)malloc(sizeof(struct node)*a);	
+
+	while(scanf("%s %d %d", nameptr, &nodeptr -> mid, &nodeptr -> fin) != EOF && i++ <=a){
+		nodeptr ->name = nameptr;
+
+		ret = (struct node **)tsearch((void *) nodeptr, (void **) &root, compare);
+		printf("\%s\"   ", (*ret)->name);
+
+
+		if(*ret == nodeptr)
+			printf("complete in tree\n");
+		else
+			printf("exist in tree \n");
+
+		nameptr += strlen(nameptr) +1;
+		nodeptr++;
+	}
+	twalk((void *) root, print_node);
+
+	free(ret);
+}
+
+void print_node(const void *nodeptr, VISIT order, int level)
+{
+	if(order == preorder || order == leaf)
+		printf("name = %20s, mid = %d, fin =%d\n",
+				(*(struct node **)nodeptr)->name,
+				(*(struct node **)nodeptr)->mid,
+				(*(struct node **)nodeptr)->fin);
+
+}
+
